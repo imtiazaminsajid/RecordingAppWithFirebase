bcc015c3edc6469c5bd98b1efb89f2008c838ef1
imtiazaminsajid <ambition.sajid@gmail.com>
2019-01-09 02:22:37 +0600

    Recording Audio Done

--- app/src/main/java/com/example/ambit/recordingappwithfirebase/MainActivity.java
+++ app/src/main/java/com/example/ambit/recordingappwithfirebase/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.ambit.recordingappwithfirebase;
 
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mRecordBtn;
+    private TextView mRecordLabel;
+
+    private MediaRecorder mRecorder;
+
+    private String mFileName = null;
+
+    private static final String LOG_TAG = "Record_log";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecordBtn = findViewById(R.id.recordBtn);
+        mRecordLabel = findViewById(R.id.recordLabel);
+
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/recoder_audio.3gp";
+
+        mRecordBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction()== MotionEvent.ACTION_DOWN){
+
+                    startRecording();
+                    mRecordLabel.setText("Recording Started...");
+
+                } else if (event.getAction() == MotionEvent.ACTION_UP){
+                    stopRecording();
+                    mRecordLabel.setText("Recording Stoped...");
+                }
+
+                return false;
+            }
+        });
     }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
 }
