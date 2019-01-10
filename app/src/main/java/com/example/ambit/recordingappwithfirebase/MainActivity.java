34d2d322a324ea3f3857b91ddf6975c4882ba0f9
imtiazaminsajid <ambition.sajid@gmail.com>
2019-01-10 13:09:26 +0600

    firebase done

--- app/src/main/java/com/example/ambit/recordingappwithfirebase/MainActivity.java
+++ app/src/main/java/com/example/ambit/recordingappwithfirebase/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.ambit.recordingappwithfirebase;
 
+import android.app.ProgressDialog;
 import android.media.MediaRecorder;
-import android.os.Environment;
-import android.os.storage.StorageManager;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,12 +32,20 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "Record_log";
 
+    StorageReference mStorage;
+
+    ProgressDialog mProgress;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        mProgress =  new ProgressDialog(this);
 
         mRecordBtn = findViewById(R.id.recordBtn);
         mRecordLabel = findViewById(R.id.recordLabel);
@@ -81,6 +97,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void uploadAudio() {
+
+        mProgress.setMessage("Uploading Audio...");
+        mProgress.show();
+
+        StorageReference filepath = mStorage.child("Audio").child("new_audio.mp3");
+
+        Uri uri =  Uri.fromFile(new File(mFileName));
+
+        filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                mProgress.dismiss();
+
+                mRecordLabel.setText("Uploding Completed...");
+
+            }
+        });
 
     }
 
